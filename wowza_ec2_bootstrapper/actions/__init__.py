47007e36e32da061c619e232196529a6720a8ddc
nocarryr <matt@nomadic-recording.com>
2015-10-13 10:15:59 -0500

    Use instance vars for actions instead of module/class level

--- wowza_ec2_bootstrapper/actions/__init__.py
+++ wowza_ec2_bootstrapper/actions/__init__.py
@@ -8,6 +8,8 @@ from set_config import SetConfig
 from customscript import CustomScript
 from cronjob import CronJob
 
+root_action = None
+
 def build_from_config(_config=None):
     if _config is None:
         _config = config
@@ -16,11 +18,15 @@ def build_from_config(_config=None):
 def save_to_config(_config=None):
     if _config is None:
         _config = config
+    if root_action is None:
+        return
     l = []
-    for action in BaseAction.all_actions:
+    for action in root_action.all_actions:
         l.append(action._serialize())
     _config.action_data['actions'] = l
     _config.to_json(indent=2)
 
 if config.action_data.get('actions'):
-    build_from_config()
+    root_action = build_from_config()
+else:
+    root_action = None
--- wowza_ec2_bootstrapper/actions/base.py
+++ wowza_ec2_bootstrapper/actions/base.py
@@ -6,17 +6,21 @@ from wowza_ec2_bootstrapper.config import config
 
 
 class BaseAction(object):
-    all_actions = []
-    action_iter = None
-    all_complete = False
-    config = config
     __action_abstract = True
     def __init__(self, **kwargs):
-        if not hasattr(self, 'action_name'):
-            self.action_name = kwargs['action_name']
-        self.kwargs = kwargs
-        self._completed = False
-        self._failed = False
+        self._root_action = kwargs.get('_root_action', self)
+        if self._root_action is self:
+            self.all_actions = []
+            self.action_iter = None
+            self.all_complete = False
+            self.config = kwargs.get('config', config)
+        else:
+            if not hasattr(self, 'action_name'):
+                self.action_name = kwargs['action_name']
+            self.kwargs = kwargs
+            self._completed = False
+            self._failed = False
+        self._root_action.all_actions.append(self)
     @classmethod
     def get_action_fields(cls, fields=None):
         if fields is None:
@@ -59,29 +63,27 @@ class BaseAction(object):
         if cls is None:
             raise Exception('Could not locate class for action %s' % (action_name))
         action = cls(**kwargs)
-        BaseAction.all_actions.append(action)
         return action
     def __call__(self):
-        if BaseAction.action_iter is None:
-            BaseAction.action_iter = iter(BaseAction.all_actions)
+        if self._root_action.action_iter is None:
+            self._root_action.action_iter = iter(self._root_action.all_actions)
         elif self.__class__ is not BaseAction:
             r = self.do_action(**self.kwargs)
             if not r:
                 self._failed = True
             self._completed = True
         try:
-            next_action = next(BaseAction.action_iter)
+            next_action = next(self._root_action.action_iter)
         except StopIteration:
             next_action = None
-            BaseAction.all_complete = True
+            self._root_action.all_complete = True
         if next_action is not None:
             next_action()
     def do_action(self):
         raise NotImplementedError('must be defined in subclass')
-    @classmethod
-    def to_json(cls, **kwargs):
+    def to_json(self, **kwargs):
         l = []
-        for action in cls.all_actions:
+        for action in self._root_action.all_actions:
             l.append(action._serialize)
         d = {'actions':l}
         return json.dumps(d, **kwargs)
@@ -103,9 +105,13 @@ class BaseAction(object):
                 data = json.loads(s)
         if isinstance(data, dict):
             data = data['actions']
+        root_action = None
         for action_kwargs in data:
-            cls.create(**action_kwargs)
-        return BaseAction
+            if root_action is not None:
+                action_kwargs.setdefault('_root_action', root_action)
+            obj = cls.create(**action_kwargs)
+            root_action = obj._root_action
+        return root_action
     def _serialize(self):
         action_name = getattr(self, 'action_name', None)
         if not action_name:
