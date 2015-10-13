dfa9d4ab5576d423e762c9acfe53a924cc38f941
nocarryr <matt@nomadic-recording.com>
2015-10-13 10:15:03 -0500

    Allow actions to use a custom config object

--- wowza_ec2_bootstrapper/actions/__init__.py
+++ wowza_ec2_bootstrapper/actions/__init__.py
@@ -8,15 +8,19 @@ from set_config import SetConfig
 from customscript import CustomScript
 from cronjob import CronJob
 
-def build_from_config():
-    return BaseAction.from_json(data=config.action_data['actions'])
+def build_from_config(_config=None):
+    if _config is None:
+        _config = config
+    return BaseAction.from_json(data=_config.action_data['actions'])
     
-def save_to_config():
+def save_to_config(_config=None):
+    if _config is None:
+        _config = config
     l = []
     for action in BaseAction.all_actions:
         l.append(action._serialize())
-    config.action_data['actions'] = l
-    config.to_json(indent=2)
+    _config.action_data['actions'] = l
+    _config.to_json(indent=2)
 
 if config.action_data.get('actions'):
     build_from_config()
