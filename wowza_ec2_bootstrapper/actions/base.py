025a1d85d9b3a1ea6392d2cccb23f53376c910df
nocarryr <matt@nomadic-recording.com>
2015-10-20 10:35:46 -0500

    Modify action init, make root_action attributes available through properties

--- wowza_ec2_bootstrapper/actions/base.py
+++ wowza_ec2_bootstrapper/actions/base.py
@@ -10,17 +10,78 @@ class BaseAction(object):
     def __init__(self, **kwargs):
         self._root_action = kwargs.get('_root_action', self)
         if self._root_action is self:
-            self.all_actions = []
-            self.action_iter = None
-            self.all_complete = False
-            self.config = kwargs.get('config', config)
-        else:
-            if not hasattr(self, 'action_name'):
-                self.action_name = kwargs['action_name']
-            self.kwargs = kwargs
-            self._completed = False
-            self._failed = False
-        self._root_action.all_actions.append(self)
+            self._all_actions = []
+            self._action_iter = None
+            self._all_complete = False
+            self._config = kwargs.get('config', config)
+        if not hasattr(self, 'action_name'):
+            self.action_name = kwargs.get('action_name', self.__class__.__name__)
+        self.kwargs = kwargs
+        self._completed = False
+        self._failed = False
+        self.all_actions.append(self)
+    @property
+    def root_action(self):
+        return self._root_action
+    @root_action.setter
+    def root_action(self, value):
+        old = self._root_action
+        if value is old:
+            return
+        if old is not None:
+            if old is self:
+                del self._all_actions
+                del self._action_iter
+                del self._all_complete
+                del self._config
+            else:
+                old.all_actions.remove(self)
+        self._root_action = value
+        if value is None:
+            return
+        value._all_actions.append(self)
+    @property
+    def all_actions(self):
+        r = self.root_action
+        if r is not None:
+            return r._all_actions
+    @property
+    def action_iter(self):
+        r = self.root_action
+        if r is not None:
+            return r._action_iter
+    @action_iter.setter
+    def action_iter(self, value):
+        if value is self.action_iter:
+            return
+        r = self.root_action
+        if r is None:
+            raise Exception('No root_action. Cannot set action_iter')
+        r._action_iter = value
+    @property
+    def all_complete(self):
+        r = self.root_action
+        if r is not None:
+            return r._all_complete
+    @all_complete.setter
+    def all_complete(self, value):
+        if value is self.all_complete:
+            return
+        r = self.root_action
+        if r is None:
+            raise Exception('No root_action. Cannot set all_complete')
+        r._all_complete = value
+    @property
+    def config(self):
+        r = self.root_action
+        if r is not None:
+            return r._config
+    @config.setter
+    def config(self, value):
+        r = self.root_action
+        if r is None:
+            raise Exception('No root_action. Cannot set config')
+        r._config = value
     @classmethod
     def get_action_fields(cls, fields=None):
         if fields is None:
@@ -65,25 +126,25 @@ class BaseAction(object):
         action = cls(**kwargs)
         return action
     def __call__(self):
-        if self._root_action.action_iter is None:
-            self._root_action.action_iter = iter(self._root_action.all_actions)
+        if self.action_iter is None:
+            self.action_iter = iter(self.all_actions)
         elif self.__class__ is not BaseAction:
             r = self.do_action(**self.kwargs)
             if not r:
                 self._failed = True
             self._completed = True
         try:
-            next_action = next(self._root_action.action_iter)
+            next_action = next(self.action_iter)
         except StopIteration:
             next_action = None
-            self._root_action.all_complete = True
+            self.all_complete = True
         if next_action is not None:
             next_action()
     def do_action(self):
         raise NotImplementedError('must be defined in subclass')
     def to_json(self, **kwargs):
         l = []
-        for action in self._root_action.all_actions:
+        for action in self.all_actions:
             l.append(action._serialize)
         d = {'actions':l}
         return json.dumps(d, **kwargs)
@@ -110,7 +171,7 @@ class BaseAction(object):
             if root_action is not None:
                 action_kwargs.setdefault('_root_action', root_action)
             obj = cls.create(**action_kwargs)
-            root_action = obj._root_action
+            root_action = obj.root_action
         return root_action
     def _serialize(self):
         action_name = getattr(self, 'action_name', None)
